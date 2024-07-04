8abd8f1270ae9ebed90cce3d3eb6d6ee8de109de
t1uman3 <tumanov.valentin@gmail.com>
2024-07-04 13:39:15 +0300

    deploy-pageX4

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function App() {
         />
         <Routes>
           <Route
-            path="react-sneakers"
+            path="react-sneakers/"
             exact
             element={
               <Home
@@ -166,12 +166,12 @@ function App() {
             }
           ></Route>
           <Route
-            path="react-sneakers/favorites"
+            path="react-sneakers/favorites/"
             exact
             element={<Favorites />}
           ></Route>
           <Route
-            path="react-sneakers/orders"
+            path="react-sneakers/orders/"
             exact
             element={<Orders />}
           ></Route>
