6361c1dddf40b2f9b304e2e07dfad631d21a61cc
t1uman3 <tumanov.valentin@gmail.com>
2024-07-04 13:16:50 +0300

    deploy-pageX3

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -9,7 +9,7 @@ const Info = ({ title, description, image }) => {
       <h2>{title}</h2>
       <p className="opacity-6">{description}</p>
       <button onClick={() => setCartOpened(false)} className="greenButton">
-        <img src="./arrow.svg" alt="arrow" />
+        <img src="img/arrow.svg" alt="arrow" />
         Return back
       </button>
     </div>
